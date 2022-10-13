ddeed67d8528796e89f36fe4278331f60508d6c3
Matt Stanbrell <mattstanbrell@gmail.com>
2022-10-13 16:04:05 +0100

    Working frequencies

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,11 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+        if str(item) in frequencies:
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1
+
     return frequencies
